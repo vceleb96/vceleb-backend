c60e72bc2300595f02c5bb0e1b500f992cd06258
Vceleb <vceleb96@gmail.com>
2026-02-02 09:45:10 +0530

    Force login success test

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,16 +1,9 @@
 import express from "express";
-import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
 router.post("/login", (req, res) => {
-  const token = jwt.sign(
-    { role: "admin" },
-    process.env.JWT_SECRET,
-    { expiresIn: "1d" }
-  );
-
-  res.json({ token });
+  res.json({ token: "TEST_TOKEN" });
 });
 
 export default router;
